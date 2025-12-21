cf78ccf0415aecb904bea9b82817057d166ee902
Igor F <igornvf@gmail.com>
2025-12-21 16:55:57 -0300

    Organizado o código e retirado códigos não utilizados

--- logreg_gd.py
+++ logreg_gd.py
@@ -4,45 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from itertools import combinations
 
-# Dataset Iris direto do sklearn
-iris = load_iris()
-
-# Matriz n x 4
-x = iris.data
-
-# Vetor com valores {0, 1, 2}
-y = iris.target
-
-# Transforma em um vetor booleano
-# (setosa é 0, se for setosa é 1, que é true)
-# Daí transforma em um problema binário
-# y = (y == 0).astype(int)
-# Como aqui a execução agora é em one-vs-all, não faz sentido fazer isso
-# Senão quebra a lógica de treinar vários modelos hahahaha
-
-# Aqui faz o split e a estraficação
-# 70 treino, 30 teste
-# (O random=42 é só pra reprodutabilidade)
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.3, random_state=42, stratify=y
-)
-
-# Escaler faz a escala do dados e ajuda na otimização
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
-
 # Função Sigmoide, como nos slides
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
-
-# Função de perda, como nos slides
-def loss(y, y_hat):
-    # epsilon assim evita o log(0)
-    eps = 1e-15
-    return -np.mean(
-        y * np.log(y_hat + eps) + (1 - y) * np.log(1 - y_hat + eps)
-    )
 
 # Treinamento por gradiente descendente, como nos slides
 # (lr: learning rate; epochs: iterações)
@@ -84,6 +48,7 @@ def train_one_vs_all(x, y, num_classes):
     return models
 
 # Treinamento do one vs one
+# Sistema de voto
 def train_one_vs_one(x, y):
     models = []
     classes = np.unique(y)
@@ -103,8 +68,15 @@ def train_one_vs_one(x, y):
 
     return models
 
-#  Treino Antigo
-#w, b = train_logistic_gd(x_train, y_train)
+# Função de predição normal, como nos slides
+# Threshold de 0.5
+# Retorna 0 ou 1
+def predict(x, w, b):
+    return (sigmoid(x @ w + b) >= 0.5).astype(int)
+# Lembrando:
+#   x -> Dados de entradas (features)
+#   w -> os pesos aprendidos pelo modelo
+#   b -> o viés (bias/intercepto)
 
 # Função de predição one-vs-all
 def predict_ova(x, models):
@@ -137,31 +109,53 @@ def predict_ovo(x, models):
 
     return np.argmax(votes, axis=1)
 
-# Função de predição antiga
-# Threshold de 0.5
-# Retorna 0 ou 1
-def predict(x, w, b):
-    return (sigmoid(x @ w + b) >= 0.5).astype(int)
-# Lembrando:
-#   x -> Dados de entradas (features)
-#   w -> os pesos aprendidos pelo modelo
-#   b -> o viés (bias/intercepto)
+# Dataset Iris direto do sklearn
+iris = load_iris()
 
-# Predição antiga
-# y_pred = predict(x_test, w, b)
+# Matriz n x 4
+x = iris.data
 
-# Numero de classes é a quantidade de itens no vetor y
-num_classes = len(np.unique(y))
+# Vetor com valores {0, 1, 2}
+y = iris.target
 
-# treina o modelo
-#models = train_one_vs_all(x_train, y_train, num_classes)
-models = train_one_vs_one(x_train, y_train)
 
-# Realiza a predição com base no one vs all
-# y_pred = predict_ova(x_test, models)
+# Aqui faz o split e a estraficação
+# 70 treino, 30 teste
+# (O random=42 é só pra reprodutabilidade)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.3, random_state=42, stratify=y
+)
 
-# Realiza a predição com base no one-vs-one
-y_pred = predict_ovo(x_test, models)
+# Escaler faz a escala do dados e ajuda na otimização
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
 
-acc = (y_pred == y_test).mean()
-print("Acurácia:", acc)
+# ---------------------------------
+
+# Treina e prediz usando a regressão logística binária
+y_train_bin = (y_train == 0).astype(int)
+y_test_bin = (y_test == 0).astype(int)
+w, b = train_logistic_gd(x_train, y_train_bin)
+y_pred = predict(x_test, w, b)
+acc = (y_pred == y_test_bin).mean()
+print("Acurácia da regressão binária:", acc)
+
+# Treina e prediz usando o One-vs-All
+num_classes = len(np.unique(y)) # Numero de classes é a quantidade de itens no vetor y
+models_ova = train_one_vs_all(x_train, y_train, num_classes)
+y_pred_ova = predict_ova(x_test, models_ova)
+acc_ova = (y_pred_ova == y_test).mean()
+print("Acurácia do One-vs-All:", acc_ova)
+
+# Treina e prediz usando o One-vs-One
+models_ovo = train_one_vs_one(x_train, y_train)
+y_pred_ovo = predict_ovo(x_test, models_ovo)
+acc_ovo = (y_pred_ovo == y_test).mean()
+print("Acurácia do One-vs-One:", acc_ovo)
+
+
+
+
+
+
